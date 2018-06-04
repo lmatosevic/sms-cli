f87703680de5db516d5a67c1a352184431a58bbe
lujo5 <lukamatosevic5@gmail.com>
2018-06-04 19:37:25 +0200

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name="sms-cli",
       author="luka",
       author_email="lukamatosevic5@gmail.com",
       url='https://github.com/Lujo5/sms-cli',
-      download_url='https://github.com/Lujo5/sms-cli/archive/sms-cli-1.0.0.tar.gz',
+      download_url='https://github.com/Lujo5/sms-cli/archive/1.0.tar.gz',
       packages=find_packages(),
       install_requires=["pyserial", "argparse"],
       entry_points={
@@ -21,5 +21,6 @@ setup(name="sms-cli",
               'sms-cli = cli.main:main'
           ]
       },
+      keywords=['sms', 'cli', 'messaging', 'gsm'],
       description="SMS command line interface tool",
       long_description=desc)
