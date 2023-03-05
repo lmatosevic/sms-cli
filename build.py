1a9f67f9bca26554add8a8a494c9a3960b00643b
lmatosevic <lukamatosevic5@gmail.com>
2023-03-05 15:59:13 +0100

    fix: update build process

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ import os
 import shutil
 
 shutil.rmtree('dist', ignore_errors=True)
-os.system('python3 setup.py sdist bdist_wheel')
+os.system('python setup.py sdist bdist_wheel')
 shutil.rmtree('sms_cli.egg-info', ignore_errors=True)
